09825983c359dbdb6295ccc8e27d7cec71484635
murat <74824857+orioninsist@users.noreply.github.com>
2026-09-18 20:30:14 +0300

    Use fd for fzf file discovery

--- .config/fzf/fzf.bash
+++ .config/fzf/fzf.bash
@@ -1,6 +1,19 @@
 # fzf default options
 export FZF_DEFAULT_OPTS_FILE="$HOME/.config/fzf/options"
 
+# Use fd for fzf file/directory discovery.
+#
+# --hidden:
+#   Include dotfiles such as .bashrc and .config.
+#
+# --exclude .git:
+#   Keep Git's internal metadata out of interactive results.
+#
+# We intentionally do not use --no-ignore, so fd continues to respect
+# .gitignore and other ignore rules.
+export FZF_CTRL_T_COMMAND='fd --hidden --exclude .git'
+export FZF_ALT_C_COMMAND='fd --hidden --type d --exclude .git'
+
 # Load fzf shell integration and keep Ctrl-R available for Atuin/Flyline.
 FZF_CTRL_R_COMMAND= eval "$(fzf --bash)"
 
