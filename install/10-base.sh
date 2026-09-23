51aa0b577943abd88369d6795b9f137643e33670
orioninsist <74824857+orioninsist@users.noreply.github.com>
2026-09-23 23:01:33 +0300

    Port install/10-base.sh to Fedora 44

--- install/10-base.sh
+++ install/10-base.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 ROOT="${DOTFILES_ROOT:?}"
-mapfile -t pkgs < <(awk -F '\t' '$1=="apt"{print $2}' "$ROOT/install/manifest.tsv")
-sudo apt-get update
-sudo apt-get install -y "${pkgs[@]}"
+
+mapfile -t pkgs < <(awk -F '\t' '$1=="dnf"{print $2}' "$ROOT/install/manifest.tsv")
+
+if (("${#pkgs[@]}" == 0)); then
+  echo "No Fedora packages found in install/manifest.tsv" >&2
+  exit 1
+fi
+
+sudo dnf -y upgrade --refresh
+sudo dnf -y install "${pkgs[@]}"
